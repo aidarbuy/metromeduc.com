0aa43efb20305bfecfdc718ea4315cb0d80b30d1
aidarbuy <aidarbuy@gmail.com>
2016-02-19 13:23:10 +0600

    Added calendar constraints

--- src/app/calendar/calendar.ctrl.js
+++ src/app/calendar/calendar.ctrl.js
@@ -4,50 +4,57 @@ export class CalendarController {
   constructor($scope, $compile, uiCalendarConfig, $firebaseObject) {
     'ngInject';
 
-    /**
-     * calendarDemoApp - 0.9.0
-     */
     var date = new Date();
     var d = date.getDate();
     var m = date.getMonth();
     var y = date.getFullYear();
-    
+
+    $scope.eventSources3 = [$scope.events3];
+
     $scope.changeTo = 'Hungarian';
  
-    /* event source that pulls from google.com */
-    // $scope.eventSource = {
-    //   url: "http://www.google.com/calendar/feeds/usa__en%40holiday.calendar.google.com/public/basic",
-    //   className: 'gcal-event',           // an option!
-    //   currentTimezone: 'America/Chicago' // an option!
-    // };
+    // event source that pulls from google.com
+    $scope.eventSource = {
+      url: "http://www.google.com/calendar/feeds/usa__en%40holiday.calendar.google.com/public/basic",
+      className: 'gcal-event',           // an option
+      currentTimezone: 'America/Chicago' // an option
+    };
 
-    /* event source that contains custom events on the scope */
+    // event source that contains custom events on the scope
     $scope.events = [
-      {title: 'All Day Event', start: new Date(y, m, d)},
-      {title: 'Long Event', start: new Date(y, m, d - 3, 11, 0), end: new Date(y, m, d - 3, 17, 30)},
-      {id: 999, title: 'Rdfdfepeating Event', start: new Date(y, m, d - 2, 17, 0), allDay: false},
-      {id: 999,title: 'Repeating Event',start: new Date(y, m, d + 4, 17, 0), allDay: false},
-      {title: 'Birthday Party',start: new Date(y, m, d + 1, 19, 0),end: new Date(y, m, d + 1, 22, 30),allDay: false},
-      {title: 'Click for Google',start: new Date(y, m, 28),end: new Date(y, m, 29),url: 'http://google.com/'}
+      {
+        id         : 'sat_off',
+        start      : '17:00',
+        end        : '20:00',
+        dow        : [6],
+        color      : '#ccc',
+        rendering  : 'background',
+        overlap    : false
+      },
+      {title: 'Patient A', start: new Date(y, m, d - 4, 11, 0), end: new Date(y, m, d - 4, 12, 0)},
+      {title: 'Patient B', start: new Date(y, m, d - 3, 14, 0), end: new Date(y, m, d - 3, 16, 30)},
+      {id: 999, title: 'Patient C', start: new Date(y, m, d - 2, 12, 0), allDay: false},
+      {id: 999, title: 'Patient D', start: new Date(y, m, d - 1, 17, 0), allDay: false},
+      {title: 'Patient E', start: new Date(y, m, d + 1, 15, 0),end: new Date(y, m, d + 1, 16, 0),allDay: false}
     ];
 
-    /* event source that calls a function on every view switch */
+    // event source that calls a function on every view switch
     $scope.eventsF = function (start, end, timezone, callback) {
       var s = new Date(start).getTime() / 1000;
       // var e = new Date(end).getTime() / 1000;
       var m = new Date(start).getMonth();
-      var events = [{title: 'Feed Me ' + m,start: s + (50000),end: s + (100000),allDay: false, className: ['customFeed']}];
+      var events = [{title: 'Feed Me ' + m, start: s + (50000), end: s + (100000), allDay: false, className: ['customFeed']}];
       callback(events);
     };
 
     $scope.calEventsExt = {
-       color: '#f00',
-       textColor: 'yellow',
-       events: [ 
-          {type:'party',title: 'Lunch',start: new Date(y, m, d, 12, 0),end: new Date(y, m, d, 14, 0),allDay: false},
-          {type:'party',title: 'Lunch 2',start: new Date(y, m, d, 12, 0),end: new Date(y, m, d, 14, 0),allDay: false},
-          {type:'party',title: 'Click for Google',start: new Date(y, m, 28),end: new Date(y, m, 29),url: 'http://google.com/'}
-        ]
+      color: '#f00',
+      textColor: 'yellow',
+      events: [ 
+        {type:'party',title: 'Lunch',start: new Date(y, m, d, 12, 0),end: new Date(y, m, d, 14, 0),allDay: false},
+        {type:'party',title: 'Lunch 2',start: new Date(y, m, d, 12, 0),end: new Date(y, m, d, 14, 0),allDay: false},
+        {type:'party',title: 'Click for Google',start: new Date(y, m, 28),end: new Date(y, m, 29),url: 'http://google.com/'}
+      ]
     };
     /* alert on eventClick */
     $scope.alertOnEventClick = function(date){
@@ -108,19 +115,21 @@ export class CalendarController {
     $scope.uiConfig = {
       calendar:{
         height: 590,
-        editable: true,
+        defaultView: 'agendaWeek',
         firstDay: 1,
+        editable: true,
+        eventConstraint: 'businessHours',
         businessHours: {
-          start: ["0.10:00:00", "5.10:00:00"], // a start time (10am)
-          // end: { days:2, hours:22, minutes:59 },
-          end: ["0.20:00:00", "5.17:00:00"] // an end time (8pm)
-          // dow: [ 0, 3, 4, 5 ] // days of week. an array of zero-based day of week integers (0=Sunday)
+          start: "10:00", // a start time (10am)
+          end: "20:00", // an end time (8pm)
+          dow: [ 1, 2, 3, 4, 5, 6 ] // days of week. an array of zero-based day of week integers (0=Sunday)
         },
         hiddenDays: [ 0 ], // hide Sundays
         minTime: "10:00:00",
         maxTime: "20:00:00",
         slotEventOverlap: false,
         slotDuration: '00:30:00',
+        eventColor: '#2cbfd9',
         // slotLabelInterval: '02:00:00',
         // selectable: true,
         header: {
@@ -132,6 +141,9 @@ export class CalendarController {
         eventDrop: $scope.alertOnDrop,
         eventResize: $scope.alertOnResize,
         eventRender: $scope.eventRender
+        // dayClick: function() {
+        //   alert('a day has been clicked!');
+        // }
       }
     };
 
@@ -146,74 +158,32 @@ export class CalendarController {
         $scope.changeTo = 'Hungarian';
       }
     };
+
     /* event sources array*/
     $scope.eventSources = [$scope.events, $scope.eventSource, $scope.eventsF];
     $scope.eventSources2 = [$scope.calEventsExt, $scope.eventsF, $scope.events];
     /* EOF */
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    /* config object */
-    this.uiConfig = {
-      calendar: {
-        height: 450,
-        editable: true,
-        customButtons: {
-          myCustomButton: {
-            text: 'custom!',
-            click: function() {
-              alert('clicked the custom button!');
-            }
-          }
-        },
-        header: {
-          left: 'month basicWeek myCustomButton agendaWeek agendaDay',
-          center: 'title',
-          right: 'today prev,next'
-        },
-        dayClick: $scope.alertEventOnClick,
-        eventDrop: $scope.alertOnDrop,
-        eventResize: $scope.alertOnResize
-      }
-    };
-
-
-    $scope.eventSources = [
+    // $scope.eventSources = [
       // your event source
-      {
-        events: [ // put the array in the `events` property
-          {
-            title: 'Event 1',
-            start: '2016-02-18'
-          },
-          {
-            title: 'event2',
-            start: '2010-01-05',
-            end: '2010-01-07'
-          },
-          {
-            title: 'event3',
-            start: '2010-01-09T12:30:00'
-          }
-        ],
-        // Event Source Options
+      // {
+      //   events: [ // put the array in the `events` property
+      //     {
+      //       title: 'Event 1',
+      //       start: '2016-02-18'
+      //     },
+      //     {
+      //       title: 'event2',
+      //       start: '2010-01-05',
+      //       end: '2010-01-07'
+      //     },
+      //     {
+      //       title: 'event3',
+      //       start: '2010-01-09T12:30:00'
+      //     }
+      //   ],
+      //   // Event Source Options
         // color: Sets every Event Object's color for this source.
         // backgroundColor: Sets every Event Object's backgroundColor for this source.
         // borderColor: Sets every Event Object's borderColor for this source.
@@ -227,12 +197,12 @@ export class CalendarController {
         // constraint: Sets every Event Object's constraint for this source.
         // allDayDefault: Sets the allDayDefault option, but only for this source.
         // eventDataTransform: Sets the eventDataTransform callback, but only for this source.
-        color: 'green',
-        textColor: 'yellow'
-      }
+      //   color: 'green',
+      //   textColor: 'yellow'
+      // }
 
       // any other event sources...
-    ];
+    // ];
 
 
     // connect to firebase
