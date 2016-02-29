6ca10655a50c5c1f8ec6367beff44efdc4b6fe58
aidarbuy <aidarbuy@gmail.com>
2016-02-29 20:38:24 +0600

    Remove ] from setHeader

--- server.js
+++ server.js
@@ -48,7 +48,7 @@ app.use(function (req, res, next) {
     // Website you wish to allow to connect
     // res.setHeader('Access-Control-Allow-Origin', '95.56.141.234');
     // res.setHeader('Access-Control-Allow-Origin', 'https://metromeduc.herokuapp.com');
-    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000']);
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
 
     // Request methods you wish to allow
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
